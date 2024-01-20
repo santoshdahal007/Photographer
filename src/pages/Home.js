6b8b4e038d274f0c1e684e719cb5be7f21124878
santoshdahal007 <santosh.dahal314@gmail.com>
2024-01-20 01:44:20 -0500

    LP-updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,19 +13,7 @@ const Home = () => {
     className='section'>
     <div className='container mx-auto h-full relative'>
       <div className='flex flex-col justify-center'>
-        <motion.div 
-          initial={{opacity:0, y:'-50%'}}
-          animate={{opacity:1, y:'0'}}
-          exit={{opacity:0, y:'-50%'}}
-          transition={{transition1}}
-          className='w-full pt-36 pb-14 lg:pt-0 
-          lg:pb-0 lg:w-auto z-10 lg:absolute
-          flex flex-col justify-center items-center lg:items-start'>
-            <h1 className='h1'>photographer <br/> & film maker</h1>
-            <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>Michigan, USA</p>
-            <Link to={'/contact'} className='btn mb-[30px]'>hire me</Link>
-        </motion.div>
-        <div className='felx justify-end max-h-96 lg:max-h-max'>
+        <div className='flex justify-end max-h-96 lg:max-h-max'>
           <motion.div 
             initial={{scale:0}}
             animate={{scale:1}}
@@ -36,9 +24,26 @@ const Home = () => {
               whileHover={{scale:1.1}}
               transition={{transition1}}
               src={WomanImg} 
-              alt='photograpers'/>
+              alt='photograper'/>
           </motion.div>
         </div>
+        <motion.div 
+          initial={{opacity:0, y:'-50%'}}
+          animate={{opacity:1, y:'0'}}
+          exit={{opacity:0, y:'-50%'}}
+          transition={{transition1}}
+          className='w-full pt-36 pb-14 lg:pt-0 
+          lg:pb-0 lg:w-auto z-10 lg:absolute
+          flex flex-col justify-center items-center 
+          lg:items-start max-w-[610px]'>
+            <h1 className='h1 mb-[30px]'>Briggett Wills</h1>
+            <p className='mb-12 dancing-script-20 sm:text-2xl'>
+              Welcome to my world of visual storytelling! I'm a 
+              passionate photographer dedicated to capturing
+              moments that resonates.
+            </p>
+            <Link to={'/contact'} className='btn mb-[30px] rounded'>Hire me</Link>
+        </motion.div>
       </div>
     </div>
   </motion.section>;

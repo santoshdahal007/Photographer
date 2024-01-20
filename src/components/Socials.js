808ac5414fdbfad079ea6cf6723d238997caa991
santoshdahal007 <santosh.dahal314@gmail.com>
2024-01-19 21:22:13 -0500

    LP-removing unwanted socials

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import {
   ImFacebook,
-  ImTwitter,
-  ImPinterest,
   ImInstagram,
-  ImYoutube
 } from 'react-icons/im';
 
 const Socials = () => {
   return <div className='hidden xl:flex ml-24'>
     <ul className='flex gap-x-4'>
       <li><ImFacebook/></li>
-      <li><ImTwitter/></li>
-      <li><ImPinterest/></li>
       <li><ImInstagram/></li>
-      <li><ImYoutube/></li>
     </ul>
   </div>;
 };

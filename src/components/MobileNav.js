29b3d3cbc84df4985f1a5de049d22aeb4b015d91
santoshdahal007 <santosh.dahal314@gmail.com>
2024-02-01 19:55:43 -0500

    new gird

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -23,37 +23,37 @@ const MobileNav = () => {
   }
 
   return <nav className='text-primary xl:hidden'>
-    <div
-      onClick={handleCgMenuClick} 
-      className='text-3xl cursor-pointer'>
-      <CgMenuRight/>
-    </div>
-    <motion.div 
-      variants={menuVariant}
-      initial='hidden'
-      animate={openMenu?'show':''}
-      className='bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20'>
       <div
         onClick={handleCgMenuClick} 
-        className='text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer'><IoMdClose/></div>
-      <ul className='
-        h-full
-        flex
-        flex-col
-        justify-center
-        items-center
-        gap-y-8
-        text-primary
-        font-primary
-        font-bold
-        text-3xl
-      '>
-        <li><Link to={'/'}>Home</Link></li>
-        <li><Link to={'/about'}>About</Link></li>
-        <li><Link to={'/portfolio'}>Portfolio</Link></li>
-        <li><Link to={'/contact'}>Contact</Link></li>
-      </ul>
-    </motion.div>
+        className='text-3xl cursor-pointer'>
+        <CgMenuRight/>
+      </div>
+      <motion.div 
+        variants={menuVariant}
+        initial='hidden'
+        animate={openMenu?'show':''}
+        className='bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20'>
+        <div
+          onClick={handleCgMenuClick} 
+          className='text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer'><IoMdClose/></div>
+        <ul className='
+          h-full
+          flex
+          flex-col
+          justify-center
+          items-center
+          gap-y-8
+          text-primary
+          font-primary
+          font-bold
+          text-3xl
+        '>
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
+          <li><Link to={'/portfolio'}>Portfolio</Link></li>
+          <li><Link to={'/contact'}>Contact</Link></li>
+        </ul>
+      </motion.div>
     </nav>;
 };
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,78 +1,98 @@
 import React from 'react';
-import Image1 from '../img/portfolio/1.png';
-import Image2 from '../img/portfolio/2.png';
-import Image3 from '../img/portfolio/3.png';
-import Image4 from '../img/portfolio/4.png';
-import { Link } from 'react-router-dom';
-import {motion} from 'framer-motion';
-import {transition1} from '../transitions';
+import Select from 'react-select'
+import { couples_engagement } from '../assets/couples';
+import LightGallery from 'lightgallery/react';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+const portfolio_options=[
+  { value: 'Back to School', label: 'Back to School' },
+  { value: 'Holiday Sessions', label: 'Holiday Sessions' },
+  { value: 'Couples/Engagement', label: 'Couples/Engagement' },
+  { value: 'Weddings', label: 'Weddings' },
+  { value: 'Maternity', label: 'Maternity' },
+  { value: 'Newborn', label: 'Newborn' },
+  { value: 'Family', label: 'Family' },
+  { value: 'Cake Smash', label: 'Cake Smash' },
+  { value: 'Kids', label: 'Kids' },
+  { value: 'Senior', label: 'Senior' },
+  { value: 'Potrait', label: 'Potrait' },
+  { value: 'Misc', label: 'Misc' }
+]
 
 const Portfolio = () => {
-  return <motion.section 
-    initial={{opacity:0, y:'100%'}}
-    animate={{opacity:1, y:0}}
-    exit={{opacity:0, y: '100%'}}
-    transition={{transition1}} 
-    className='section'>
-    <div className='container mx-auto h-full relative'>
-      <div className='flex flex-col lg:flex-row h-full
-        items-center justify-start gap-x-24 text-center
-        lg:text-left pt-24 lg:pt-36 pb-8'>
-        <motion.div 
-          initial={{opacity:0, y:'-80%'}}
-          animate={{opacity:1, y:0}}
-          exit={{opacity:0, y: '-80%'}}
-          transition={{transition1}}
-          className='flex flex-col lg:items-start'>
-          <h1 className='h1'>Portfolio</h1>
-          <p className='mb-12 max-w-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-            incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu 
-            non diam phasellus vestibulum. Consectetur libero id faucibus nisl tincidunt eget.
-            Magna fermentum iaculis eu non. Nisi lacus sed viverra tellus in hac.
-          </p>
-          <Link to={'/contact'} className='btn mb-[30px] mx-auto lg:mx-0 rounded-full'>Hire me</Link>
-        </motion.div>
-        <div className='grid grid-cols-2 lg:gap-2'>
-          <div className='max-w-[250px] lg:max-w-[320px] h-[187px]
-            lg:h-[220px] bg-accent overflow-hidden'>
-            <img 
-              className=' object-cover h-full lg:h-[220px]
-                hover:scale-110 transition-all duration-500'
-              src={Image1}
-              alt=''
-              />
-          </div>
-          <div className='max-w-[250px] lg:max-w-[320px] h-[187px]
-            lg:h-[220px] bg-accent overflow-hidden'>
-            <img 
-              className=' object-cover h-full lg:h-[220px]
-                hover:scale-110 transition-all duration-500'
-              src={Image2}
-              alt=''
-              />
-          </div>
-          <div className='max-w-[250px] lg:max-w-[320px] h-[187px]
-            lg:h-[220px] bg-accent overflow-hidden'>
-            <img 
-              className=' object-cover h-full lg:h-[220px]
-                hover:scale-110 transition-all duration-500'
-              src={Image3}
-              alt=''
-              />
-          </div>
-          <div className='max-w-[250px] lg:max-w-[320px] h-[187px]
-            lg:h-[220px] bg-accent overflow-hidden'>
-            <img 
-              className=' object-cover h-full lg:h-[220px]
-                hover:scale-110 transition-all duration-500'
-              src={Image4}
-              alt=''
-              />
-          </div>
+  const test_pos=[];
+  let count = 0
+  const test = (rectValue,index,el)=>{
+    if(count === 4){
+      count = 0;
+    }
+    if(!test_pos[count]){
+      test_pos[count] =[rectValue.bottom,index];
+      count += 1;
+    }
+    else if(test_pos[count]?.[1] !== index){
+      const posDiff = rectValue.top - test_pos[count][0];
+      if(posDiff > 16){
+        const new_pos = posDiff - 16;
+        el.style.top = `-${new_pos}px`;
+        test_pos[count] = [(rectValue.bottom - new_pos),index]
+      }else{
+        test_pos[count] = [rectValue.bottom,index];
+      }
+      count += 1;
+    }
+  }
+
+  return( 
+    <section 
+      className='top-[120px] lg:top-[140px] absolute 
+      w-full px-[30px] lg:px-[100px] section'>
+      <div>
+        <Select className='block xl:hidden' options={portfolio_options} />
+        <ul className='hidden xl:flex flex-col 
+          lg:flex-row grow shrink gap-x-4'>
+          {
+            portfolio_options.map((val)=>{
+              return <li className='h-auto'>
+                  <p className='text-md text_color'>{val.label}</p>
+                </li>
+            })
+          }
+        </ul>
+      </div>
+      <div className="App overflow-scroll h-3/4">
+        <div className='h-[98%]'>
+          <LightGallery
+            speed={500}
+            plugins={[lgThumbnail, lgZoom]}
+            elementClassNames='grid grid-cols-4 gap-4'>
+            {
+              couples_engagement.map((img,index)=>{
+                return (
+                  <a href={img}>
+                    <img 
+                      ref={el=>{
+                        if(!el) return;
+                        test(el.getBoundingClientRect(),index,el);
+                      }}
+                      alt="img1" 
+                      src={img} 
+                      style={{position:'relative'}}
+                      width="400px"/>
+                  </a>
+                )
+              })
+            }
+          </LightGallery>
         </div>
       </div>
-    </div>
-  </motion.section>;
+    </section>
+  )
 };
 
 export default Portfolio;
